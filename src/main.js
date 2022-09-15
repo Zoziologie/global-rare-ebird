3d9fb9e0b984e9b82ac9dbd1c550697c554bec1b
rafnuss <rafnuss@gmail.com>
2022-09-15 18:30:18 -0400

    move style in a seperate file

--- src/main.js
+++ src/main.js
@@ -16,13 +16,13 @@ Vue.component("multiselect", Multiselect);
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faDirections, faClone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 library.add(faDirections, faClone);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 import VueCookie from "vue-cookie";
-
 Vue.use(VueCookie);
+
+import './style.scss'
 
 new Vue({
   render: (h) => h(App),
