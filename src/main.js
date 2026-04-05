6840b60ef0f2c539e8b196b7b7b697ff04855dea
Raphaël Nussbaumer <rafnuss@gmail.com>
2026-04-05 21:45:15 +0200

    upgrade pacakge

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "mapbox-gl/dist/mapbox-gl.css"
 
-import "./style.scss"
+import "./style.css"
 import App from "./App.vue"
 
 const app = createApp(App)
